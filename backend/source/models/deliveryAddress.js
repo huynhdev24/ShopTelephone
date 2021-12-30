a38eb45d844fa31ec70236a670b7de072ec0dfcc
Ba Hoang <phanbahoang10@gmail.com>
2021-12-30 11:29:06 -0800

    add deliveryAddressModel

---
+++ backend/source/models/deliveryAddress.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const deliveryAddressSchema = mongoose.Schema({
+    user          : { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User', },
+  list: {
+    type: Array,
+    // tên người nhận
+    name: { type: String, required: true, trim: true, maxLength: 40 },
+    phone: { type: String, required: true, trim: true, maxLength: 10 },
+    // địa chỉ
+    address: {
+      type: Object,
+      required: true,
+      province: String,
+      district: String,
+      wards: String,
+      details: { type: String, default: '' },
+    },
+  },
+});
+
+//create Model
+const DeliveryAddress = mongoose.model('deliveryAddress',deliveryAddressSchema);
+
+export default DeliveryAddress
+
